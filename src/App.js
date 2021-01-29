4e33b78952a969666b06bc23a38e758f3b1a3536
Joel Lavaisse <joel.lavaisse@gmail.com>
2021-01-29 01:09:10 -0300

    Add switch button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import Main from './components/Main';
 import Button from './components/Button';
+import Container from './components/Container';
 
 function App() {
+  const [isActive, setIsActive] = useState(true);
+
   return (
     <Main className="App">
-      <Button round>OK</Button>
-      <Button>Cancel</Button>
+      <Container>
+        <Button round size="small">OK</Button>
+        <Button round>OK</Button>
+        <Button round size="large">OK</Button>
+        <Button round size="large" type="switch" active={isActive} onClick={() => setIsActive(!isActive)} />
+      </Container>
+      <Container>
+        <Button size="small">OK</Button>
+        <Button>OK</Button>
+        <Button size="large">OK</Button>
+      </Container>
     </Main>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,27 @@
 import styled from 'styled-components'
 
-const mainSize = '3em'
+const mainSize = '2.5em';
+const getSize = (size) => {
+  const sizes = {
+    small: '14px',
+    normal: '18px',
+    large: '24px',
+  };
+  return sizes[size] || sizes.normal;
+}
 
-const Button = styled.button`
+const ButtonText = styled.div`
+  background-image: linear-gradient(135deg, rgba(173,175,188,1) 0%, rgba(93,94,102,1) 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
+const ButtonBody = styled.button`
   height: ${mainSize};
   width: ${props => props.round && mainSize};
+  min-width: ${props => !props.round && `calc(${mainSize} * 2)`};
   font-weight: 600;
-  font-size: 22px;
+  font-size: ${props => getSize(props.size)};
   user-select: none;
   border-radius: ${props => props.round ? '50%' : `calc(${mainSize} / 2)`};
   padding: 0 ${props => !props.round && `calc(${mainSize} / 2)`};
@@ -27,5 +42,34 @@ const Button = styled.button`
       color: #7e7f7a;
     }
 `;
+
+const ButtonLight = styled.div`
+  background: ${props => props.active ? '#1eff0a' : '#fa000b'};
+  border-radius: 50%;
+  height: ${props => getSize(props.size)};
+  width: ${props => getSize(props.size)};
+  box-shadow:
+        3px 3px 3px 0 rgba(0, 0, 0, 0.5) inset,
+        -6px -6px 12px 0 rgba(255, 255, 255, 0.025) inset;
+  
+  &:active {
+    background: #0fb300;
+  }
+`;
+
+
+const Button = (props) => {
+  return (
+    <ButtonBody round={props.round} size={props.size} onClick={props.onClick}>
+      {props.type === 'switch' ?
+        <ButtonLight active={props.active}/>
+        :
+        <ButtonText props={props}>
+          {props.children}
+        </ButtonText>
+      }
+    </ButtonBody>
+  )
+}
 
 export default Button;
