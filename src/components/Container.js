f843c977cd8161c135710df6ce1db01957b7326a
Joel Lavaisse <joel.lavaisse@gmail.com>
2021-01-29 01:09:22 -0300

    Add simple flex container

---
+++ src/components/Container.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  min-width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+export default Container;
